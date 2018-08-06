e142a303c0d25517c9afda6ba606664dc2ce2144
LUM XU FENG <16043921@rp.edu.sg>
2018-08-06 21:15:40 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/dynamicspinner/MainActivity.java
@@ -0,0 +1,105 @@
+package sg.edu.rp.c346.dynamicspinner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    Spinner spn1;
+    Spinner spn2;
+    Button btnUpdate;
+
+    ArrayAdapter<String> aaNumbers;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spn1 = findViewById(R.id.spinner1);
+        spn2 = findViewById(R.id.spinner2);
+        btnUpdate = findViewById(R.id.buttonUpdate);
+        final ArrayList<String> alNumbers;
+        alNumbers = new ArrayList<>();
+
+        //Get the string-array and store as an Array
+        String[] strNumbers = getResources().getStringArray(R.array.even_numbers);
+
+        //Convert Array to List and add to the ArrayList
+        alNumbers.addAll(Arrays.asList(strNumbers));
+
+
+
+
+        aaNumbers = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, alNumbers);
+
+        spn2.setAdapter(aaNumbers);
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int pos = spn1.getSelectedItemPosition();
+                alNumbers.clear();
+
+                if (pos == 0){
+                    //Get the string-array and store as an Array
+                    String[] strNumbers = getResources().getStringArray(R.array.even_numbers);
+
+                    //Convert Array to List and add to the ArrayList
+                    alNumbers.addAll(Arrays.asList(strNumbers));
+                }
+                else if (pos == 1){
+                    //Get the string-array and store as an Array
+                    String[] strNumbers = getResources().getStringArray(R.array.odd_numbers);
+
+                    //Convert Array to List and add to the ArrayList
+                    alNumbers.addAll(Arrays.asList(strNumbers));
+                }
+
+
+            }
+        });
+
+        spn1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch(i){
+                    case 0:
+                        alNumbers.clear();
+                        //Get the string-array and store as an Array
+                        String[] strNumbers = getResources().getStringArray(R.array.even_numbers);
+
+                        //Convert Array to List and add to the ArrayList
+                        alNumbers.addAll(Arrays.asList(strNumbers));
+                        spn2.setSelection(2);
+                        break;
+                    case 1:
+                        alNumbers.clear();
+                        //Get the string-array and store as an Array
+                        String[] strNumbers1 = getResources().getStringArray(R.array.odd_numbers);
+
+                        //Convert Array to List and add to the ArrayList
+                        alNumbers.addAll(Arrays.asList(strNumbers1));
+                        spn2.setSelection(1);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+    }
+}
